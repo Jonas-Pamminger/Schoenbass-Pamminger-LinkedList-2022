1f0b40b4c472126c28782da55e7e7d9926840282
JonasSchoenbass <91074631+JonasSchoenbass@users.noreply.github.com>
2022-04-26 16:11:33 +0200

    next

--- main.c
+++ main.c
@@ -87,23 +87,14 @@ void insertStart(struct Node** head, int data)
 
 void insertAfter(struct Node* prev_node, int new_data)
 {
-    /*1. check if the given prev_node is NULL */
-    if (prev_node == NULL) {
+    if (prev_node == NULL) 
+    {
         printf("the given previous node cannot be NULL");
         return;
     }
-
-    /* 2. allocate new node */
-    struct Node* new_node
-        = (struct Node*)malloc(sizeof(struct Node));
-
-    /* 3. put in the data */
+    struct Node* new_node= (struct Node*)malloc(sizeof(struct Node));
     new_node->data = new_data;
-
-    /* 4. Make next of new node as next of prev_node */
     new_node->next = prev_node->next;
-
-    /* 5. move the next of prev_node as new_node */
     prev_node->next = new_node;
 }
 
